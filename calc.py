cfe7588bc95637d0c7dc05fb94d1e9f0b14c56f5
Reiszin01 <ayurireis@alu.ufc.br>
2025-05-14 16:35:40 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ def sub(x, y):
     return x - y
 def sum(x, y):
         """Função que retorna a soma de x e y."""
-    return x + y + 1
+    return x + y
  def div(x, y):
       return x / y if y != 0 else None
